fac18a5732185bd0050534b88c5e79d022f514e4
zaruyashar <203256006+zaruyashar@users.noreply.github.com>
2026-06-21 09:29:54 +0300

    shared layout templates for the frontend & admin dashboard created

---
+++ MOVIFY/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using MOVIFY.Models;
+using System.Diagnostics;
+
+namespace MOVIFY.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- MOVIFY/Program.cs
+++ MOVIFY/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MOVIFY.Data.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.
+UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 

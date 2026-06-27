5eb322888190d21109f32744fe5aaac28245e085
zaruyashar <203256006+zaruyashar@users.noreply.github.com>
2026-06-27 23:57:00 +0300

    minor style changes

--- MOVIFY/Controllers/DefaultController.cs
+++ MOVIFY/Controllers/DefaultController.cs
@@ -1,15 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
-using MOVIFY.Models;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using MOVIFY.Data.Data;
+using System.Linq;
 
 namespace MOVIFY.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public DefaultController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
+        public IActionResult Index()
+        {
+            var latestMovies = _context.Movies
+                .Include(m => m.Category)
+                .Include(m => m.Director)
+                .OrderByDescending(m => m.MovieId)
+                .Take(6)
+                .ToList();
+
+            return View(latestMovies);
+        }
+
+        public IActionResult Search(string keyword)
+        {
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                return RedirectToAction("Index");
+            }
+
+            var movies = _context.Movies
+                .Include(m => m.Category)
+                .Include(m => m.Director)
+                .Where(m => m.MovieTitle.Contains(keyword) ||
+                            m.Category.CategoryName.Contains(keyword) ||
+                            m.Director.DirectorFullName.Contains(keyword))
+                .ToList();
+
+            ViewBag.Keyword = keyword;
+            return View(movies);
+        }
     }
 }
